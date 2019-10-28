307458b930d76ed4e9c7b078944bd936e050debc
Tylarobson <3robst58@solent.ac.uk>
2019-10-28 12:09:21 +0000

    TCA

---
+++ week 3/main.py
@@ -0,0 +1,11 @@
+print("I wonder what is in my suitcase...")
+suitcase = input()
+
+if (suitcase == "toothbrush"):
+    print ("A toothbrush. Well, got to have clean teeth!")
+elif (suitcase == "spidey suit"):
+    print("My spidey suit! i wont be needing this. I am on holiday.")
+else:
+    print("An unexspected item! it could be useful")
+
+print ()
