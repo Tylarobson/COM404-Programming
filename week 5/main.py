7e4983b4a217eacbf429f7f29239e41ed39d3084
Tylarobson <3robst58@solent.ac.uk>
2019-10-28 12:51:23 +0000

    question5

---
+++ week 5/main.py
@@ -0,0 +1,9 @@
+print ("How many zones must i cross?")
+zones = int(input))
+
+print()
+
+for count in range (zones, 0, -1):
+    print("..crossed zones", count)
+
+print("Crossed all zones. Jumanji")
