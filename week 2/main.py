4b53af3850ba73548707a6a8052d12aeabade2f9
Tylarobson <3robst58@solent.ac.uk>
2019-10-28 11:44:44 +0000

    question2

---
+++ week 2/main.py
@@ -0,0 +1,9 @@
+print ("where is forky")
+forky = input()
+
+if (forky == "with bonnie"):
+    print("\nPhew! bonnie will be happy!")
+elif (forky == "running away"):
+    print ("\nOh no! bonnie will be upset")
+else:
+    print("\nAh, i better go and look for him")
